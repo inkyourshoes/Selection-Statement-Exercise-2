391a89596aed332b04b9bfac789e8af779a64bce
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2025-07-14 20:34:22 -0400

    second one too

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+            
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I like some math!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is my favorite subject!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is fun when you can be creative!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is boring as a school class!");
+                    break;
+                case "pe":
+                case "p.e.":
+                case "physical education":
+                    Console.WriteLine("PE is my favorite subject!");
+                    break;
+                default:
+                    Console.WriteLine("I like that too!");
+                    break;
+            }
         }
     }
 }
